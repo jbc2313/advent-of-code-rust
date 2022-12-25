040d8ce15d6a33bfd679b3b249f73ef426bcb160
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-25 10:18:16 -0600

    remove target

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/jimbriar/mycode/rust/advent_of_code/day1/target/debug/day1: /Users/jimbriar/mycode/rust/advent_of_code/day1/src/main.rs
--- day1/target/debug/deps/day1-6664b9d6224703ab.d
+++
@@ -1,5 +0,0 @@
-/Users/jimbriar/mycode/rust/advent_of_code/day1/target/debug/deps/day1-6664b9d6224703ab.rmeta: src/main.rs
-
-/Users/jimbriar/mycode/rust/advent_of_code/day1/target/debug/deps/day1-6664b9d6224703ab.d: src/main.rs
-
-src/main.rs:
