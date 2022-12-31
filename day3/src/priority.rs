69cf45c02082cc8ec1a77a249dac1ba8080d984c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-31 15:27:05 -0600

    hashmap for uppercase init

--- day3/src/priority.rs
+++ day3/src/priority.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::any::type_name;
 
 pub fn letter_priority() {
     let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -8,7 +9,25 @@ pub fn letter_priority() {
         lowercase.insert(String::from(lower[idx-1]), idx.try_into().unwrap());
     }
 
-    println!("{:?}", lowercase);
+    // not in order
+    //println!("{:?}", lowercase);
+    // not in order here either  hashmap retur in an arbitrary order...
+
+    for (key, value) in &lowercase {
+        println!("{key}: {value}");
+    }
+    
+    // need to change lowercase letters to uppercase.
+    // then add them to a hashmap like the above.
+    
+    let mut upper = &lower;
+
+    // for el in upper {
+    //     println!("{:?}", print_type_of(el));
+    //
+    // }
+    
+    let mut uppercase: HashMap<String, i32> = HashMap::new();
 
 
 
