f370d399048e5413c057b1dc77c31d63b44084b6
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-01 14:56:05 -0600

    hashmap for uppercase letters finished;

--- day3/src/priority.rs
+++ day3/src/priority.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 use std::any::type_name;
 
+pub fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
 pub fn letter_priority() {
     let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let mut lowercase: HashMap<String, i32> = HashMap::new();
@@ -16,19 +20,23 @@ pub fn letter_priority() {
     for (key, value) in &lowercase {
         println!("{key}: {value}");
     }
-    
-    // need to change lowercase letters to uppercase.
-    // then add them to a hashmap like the above.
-    
-    let mut upper = &lower;
 
-    // for el in upper {
-    //     println!("{:?}", print_type_of(el));
+  
+    let mut uppercase: HashMap<String, i32> = HashMap::new();
+    
+    for idx in 1..=26 {
+        uppercase.insert(String::from(lower[idx-1].to_uppercase()), (idx + 26).try_into().unwrap());
+    }
+
+    println!("{:?}", uppercase); 
+
+       // for el in upper {
+    //     //println!("{:?}", print_type_of(&el));
+    //     //let el = &el.to_uppercase();
+    //     println!("{:?}", el);
     //
     // }
     
-    let mut uppercase: HashMap<String, i32> = HashMap::new();
-
 
 
 }
